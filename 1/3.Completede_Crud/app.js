6e4f47f7d423b55bb19a86d6916b9c09b897a1ba
dacho-J <joketovicdaniel@gmail.com>
2021-03-17 20:53:47 +0100

    Completede CRUD API

--- 1/3.Completede_Crud/app.js
+++ 1/3.Completede_Crud/app.js
@@ -1,22 +1,62 @@
+const { json } = require("express");
 const express = require("express");
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req,res) => {
     res.send('Welcome to this Node.js API');
 });
 
-const users = [
-    {id:1, firstName: 'Daniel', lastName: 'Joketovic' },
-    {id:2, firstName: 'thomas', lastName: 'Jesperne' },
-    {id:3, firstName: 'Anna', lastName: 'Frydendal' },
-    {id:4, firstName: 'Jens', lastName: 'Jakobsen' },
-    {id:5, firstName: 'Caroline', lastName: 'Petersen'},
-    {id:6, firstName: 'Diana', lastName: 'Lækkersen' }
+let users = [
+    {id:1, firstName: 'Daniel', occupation: 'Computer scientist' },
+    {id:2, firstName: 'thomas', occupation: 'Soldier' },
+    {id:3, firstName: 'Anna', occupation: 'Baker' },
+    {id:4, firstName: 'Jens', occupation: 'Machine Engineer' },
+    {id:5, firstName: 'Caroline', occupation: 'Candidate'},
+    {id:6, firstName: 'Diana', occupation: 'Woodchopper' }
 ]
 
+let id = users.length;
 
 app.get('/users',(req, res) =>{
-    res.send(users);
+    res.send({data: users});
+});
+
+app.get("/users/:id",(req, res) =>{
+    const userId = Number(req.params.id);
+    const foundUsers = users.find(user => user.id === userId);
+    res.send({foundUsers: foundUsers})
+
+});
+
+app.post("/users", (req, res) =>{
+    const newUser = req.body;
+    console.log(newUser);
+    newUser.id = ++id;
+    users.push(newUser)
+    console.log(newUser);
+    //res.send er det som jeg forventer at få tilbage som svar fra API (i postman)
+    res.send({msg: `ny bruger med id ${newUser.id} and name is ${newUser.firstName}`})
+});
+
+app.patch("/users/:id", (req, res) => {
+    let userUpdated = false;
+    const userId = req.body;
+    users = users.map(user => {
+        if(user.id === Number(req.params.id)){
+            userUpdated = true;
+            return{...user, ...req.body, id: user.id};
+        }
+        return user;
+    });
+    res.send({data : userUpdated, msg:`user with the user id: ${userId.id} has been changed`});
+});
+
+app.delete("/users/:id",(req, res) =>{
+    const userId = req.body;
+    users = users.filter(user => user.id !== Number(req.params.id));
+    res.send({ msg:`user with the id: ${userId.id} has been deleted.`})
 })
 
 
